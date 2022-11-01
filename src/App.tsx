9b083e0d9e7db48b272cf2aafe4799676a3658da
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-01 09:42:14 -0300

    ADionando a rota para a tela dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import { Home } from './Pages/Home';
+import { HomeDark } from './Pages/HomeDark';
 import { PageNotFound } from './Pages/PageNotFound';
 
 export function App() {
@@ -8,6 +9,9 @@ export function App() {
         <Routes>
           <Route path='/' element={<Home />} />
             <Route path='pageNotFound' element={<PageNotFound />} />
+            <Route path='homeDark' element={<HomeDark />} />
+              <Route path='pageNotFound' element={<PageNotFound />} />
+            <Route />
           <Route />
         </Routes>
       </BrowserRouter>
