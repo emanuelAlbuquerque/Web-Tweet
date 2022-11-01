9a082416ce03b7a9a0af9fc0e4c3e933d9ce061a
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-01 09:01:52 -0300

    Página de erro 404

---
+++ src/Pages/PageNotFound.tsx
@@ -0,0 +1,16 @@
+import logo from '../assets/logo-twitter.webp'
+
+export function PageNotFound(){
+  return(
+    <div className="h-[100vh]">
+      <header className="bg-primary-blue h-[25%] flex items-center justify-center">
+        <h1 className="text-dark-8 text-40 uppercase font-700 tracking-[.2em]">Twitter</h1>
+      </header>
+      <main className="h-[75%] flex flex-col items-center pt-12">
+        <h2 className='text-black font-700 text-40'>Error 404 - Page not found</h2>
+        <img src={logo} alt="" width={200} height={200} className='mt-8'/>
+        <p className='mt-8 text-dark-3 text-19'>Descupe! Essa página não existe</p>
+      </main>
+    </div>
+  )
+}
