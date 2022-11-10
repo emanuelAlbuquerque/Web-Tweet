2df933e73737d821ac9dd390d279c532cf19c845
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-10 09:34:14 -0300

    Ajeitando o nome da classe bg-primary-blue

--- src/Pages/PageNotFound.tsx
+++ src/Pages/PageNotFound.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo-twitter.webp'
 export function PageNotFound(){
   return(
     <div className="h-[100vh]">
-      <header className="bg-primary-blue h-[25%] flex items-center justify-center">
+      <header className="bg-primary-blue-active h-[25%] flex items-center justify-center">
         <h1 className="text-dark-8 text-40 uppercase font-700 tracking-[.2em]">Twitter</h1>
       </header>
       <main className="h-[75%] flex flex-col items-center pt-12">
