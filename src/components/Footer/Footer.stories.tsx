a2ee0caabe99a4c0ac2fe54316cc919bcd8167d4
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-10 09:46:48 -0300

    Adicionando a funcionalidade de login do usuário, adicionando o modo dark

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -1,9 +1,44 @@
-import { Footer } from "./Footer";
+import { Footer, FooterProps } from "./Footer";
 import { Meta, StoryObj } from '@storybook/react';
+import { withRouter } from 'storybook-addon-react-router-v6'
 
 export default {
   title: 'Components/Footer',
   component: Footer,
-} as Meta
+  decorators: [withRouter],
+} as Meta<FooterProps>
 
-export const Default: StoryObj = {}
+export const FooterLight: StoryObj<FooterProps> = {
+  args: {
+    usuarioLogado: {
+      logado: false,
+      idUser: ''
+    }
+  }
+}
+export const FooterDark: StoryObj<FooterProps> = {
+  args: {
+    isDark: true,
+    usuarioLogado: {
+      logado: false,
+      idUser: ''
+    }
+  }
+}
+export const FooterLightLogado: StoryObj<FooterProps> = {
+  args: {
+    usuarioLogado: {
+      logado: true,
+      idUser: ''
+    }
+  }
+}
+export const FooterDarkLogado: StoryObj<FooterProps> = {
+  args: {
+    isDark: true,
+    usuarioLogado: {
+      logado: true,
+      idUser: ''
+    }
+  }
+}
