de405c1e3e82f802ee3b9ab6b509d23383f39e32
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-11-10 10:31:19 -0300

    Alterando a pagina home para mostrar alguns components apenas quando o usuário for logado e fazendo a lógica para cadastro de publicações no local storage

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -105,7 +105,7 @@ export function Home(){
               <Header.Title>
                 Home
               </Header.Title>
-              <Header.Icon link='homeDark'>
+              <Header.Icon>
                 <IconHeader />
               </Header.Icon>
             </Header.Root>
